9b00fa42cb873e1bcbc852be0c620ad054b77164
Nour-1412 <nourelain796@gmail.com>
2026-08-02 16:05:16 +0300

    Refactor navbar HTML structure in app.js

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
+/* ==============================
+      WebBag Aurora UI v2
+============================== */
+
 const navbar = document.getElementById("navbar");
 
 navbar.innerHTML = `
 
 <div class="brand">
 
-WebBag
+    WebBag
 
 </div>
 
 <div class="nav-menu">
 
-<a href="#">الرئيسية</a>
+    <a href="#">الرئيسية</a>
 
-<a href="#">الأدوات</a>
+    <a href="#">الأدوات</a>
 
-<a href="#">المميزات</a>
+    <a href="#">المميزات</a>
 
-<a href="#">الأسعار</a>
+    <a href="#">الأسعار</a>
 
-<button class="pro-btn">
+    <button class="pro-btn">
 
-WebBag Pro
+        WebBag Pro
 
-</button>
+    </button>
 
 </div>
 
 `;
-
