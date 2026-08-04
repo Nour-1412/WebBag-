598a8faec151060c13c2e03772dae8b4ce451f89
Nour-1412 <nourelain796@gmail.com>
2026-08-04 18:15:28 +0300

    Add theme toggle feature with local storage
    Implement theme toggle functionality with local storage support.

--- app.js
+++ app.js
@@ -339,3 +339,40 @@ display:block;
 margin:auto;
 
 }
+/* ==========================================
+        Theme Mode
+========================================== */
+
+const themeBtn =
+document.getElementById("themeToggle");
+
+const savedTheme =
+localStorage.getItem("theme");
+
+if(savedTheme==="dark"){
+
+document.body.classList.add("dark");
+
+themeBtn.textContent="☀️";
+
+}
+
+themeBtn.addEventListener("click",()=>{
+
+document.body.classList.toggle("dark");
+
+if(document.body.classList.contains("dark")){
+
+themeBtn.textContent="☀️";
+
+localStorage.setItem("theme","dark");
+
+}else{
+
+themeBtn.textContent="🌙";
+
+localStorage.setItem("theme","light");
+
+}
+
+});
